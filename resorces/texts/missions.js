2e654d6774cff4da4150348fc56a999ff13586d9
Suxton <suxton@icloud.com>
2022-07-22 14:55:47 +0800

    task_list ver 1.0

---
+++ resorces/texts/missions.js
@@ -0,0 +1,26 @@
+var missi = [{
+  roleName: "avava",
+  missionShown: 3,
+  missionList: [{
+    text: "前往侍女馆找梁老师",
+    status: "block"
+  }, {
+    text: "接下梁老师手上的狗链",
+    status: "block"
+  }, {
+    text: "套上狗链，成为梁老师的狗",
+    status: "block"
+  }, {
+    text: "jogggjojo",
+    status: "none"
+  }, {
+    text: "jojogggdjo",
+    status: "none"
+  }]
+}, {
+
+}];
+
+module.exports = {
+  missions: missi
+}
